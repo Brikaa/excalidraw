57fbf3aaf195532459a5209792534949d9c43953
Omar Brikaa <brikaaomar@gmail.com>
2025-07-14 23:20:04 +0300

    Initial auto-save indicator

--- excalidraw-app/auto-save/AutoSave.tsx
+++ excalidraw-app/auto-save/AutoSave.tsx
@@ -1,6 +1,7 @@
-import { memo, useEffect, useRef } from "react";
+import { memo, useCallback, useEffect, useRef, useState } from "react";
 import { FileHandleIDB } from "excalidraw-app/data/LocalData";
 import { debounce } from "@excalidraw/common";
+import { ExportIcon } from "@excalidraw/excalidraw/components/icons";
 
 import type { FileSystemHandle } from "@excalidraw/excalidraw/data/filesystem";
 import type { ExcalidrawImperativeAPI } from "@excalidraw/excalidraw/types";
@@ -11,10 +12,12 @@ const save = debounce(
   async (
     excalidrawAPI: ExcalidrawImperativeAPI,
     fileHandle: FileSystemHandle,
+    showIndicator: () => void,
   ) => {
     const permission = await fileHandle.queryPermission({ mode: "readwrite" });
     if (permission !== "granted") {
-      console.log("Need to show indicator");
+      console.log("Showing indicator");
+      showIndicator();
     } else {
       try {
         await excalidrawAPI.saveToFile();
@@ -39,25 +42,58 @@ export const AutoSave = memo((props: AutoSaveProps) => {
   const { excalidrawAPI } = props;
   const fileHandle = useRef(excalidrawAPI.getAppState().fileHandle);
 
+  const [showIndicator, setShowIndicator] = useState(false);
+
+  const handleIndicatorClick = useCallback(async () => {
+    if (fileHandle.current) {
+      const permission = await fileHandle.current.requestPermission({
+        mode: "readwrite",
+      });
+      if (permission === "granted") {
+        await excalidrawAPI.saveToFile();
+        setShowIndicator(false);
+      }
+    } else {
+      await excalidrawAPI.saveToFile();
+      setShowIndicator(false);
+    }
+  }, [excalidrawAPI]);
+
   useEffect(() => {
     const unsubOnChange = excalidrawAPI.onChange(async (_, appState) => {
       if (appState.fileHandle !== fileHandle.current && !appState.isLoading) {
         await FileHandleIDB.save(appState.fileHandle);
         fileHandle.current = appState.fileHandle;
+        if (appState.fileHandle === null) {
+          setShowIndicator(false);
+        }
         console.log("Changed file handle");
       }
     });
 
+    return unsubOnChange;
+  }, [excalidrawAPI]);
+
+  useEffect(() => {
     const unsubOnIncrement = excalidrawAPI.onIncrement((event) => {
-      if (fileHandle.current && Object.keys(event.change.elements).length) {
-        save(excalidrawAPI, fileHandle.current);
+      if (
+        fileHandle.current &&
+        Object.keys(event.change.elements).length &&
+        !showIndicator
+      ) {
+        save(excalidrawAPI, fileHandle.current, () => setShowIndicator(true));
       }
     });
+    return unsubOnIncrement;
+  }, [excalidrawAPI, showIndicator]);
 
-    return () => {
-      unsubOnChange();
-      unsubOnIncrement();
-    };
-  }, [excalidrawAPI]);
-  return null;
+  return (
+    <>
+      {showIndicator && (
+        <div className="auto-save-indicator" onClick={handleIndicatorClick}>
+          {ExportIcon}
+        </div>
+      )}
+    </>
+  );
 });
